33909f159c1549719a16f32975b1ee9fd41d9853
Mantas <syenergyy@gmail.com>
2020-01-25 03:23:31 +0200

    Add files via upload

---
+++ PH_Subtitles/PH_Subtitles.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using HarmonyLib;
+using BepInEx;
+
+using BepInEx.Harmony;
+using UnityEngine;
+using UnityEngine.UI.Translation;
+
+namespace PH_Subtitles {
+    [BepInPlugin(nameof(PH_Subtitles), nameof(PH_Subtitles), "1.0.0")]
+    public class PH_Subtitles : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            HarmonyWrapper.PatchAll(typeof(PH_Subtitles));
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(AudioSource), "Play", new Type[]{})]
+        public static void AudioSource_Play_Patch(AudioSource __instance)
+        {
+            SetSubtitle(__instance);
+        }
+
+        private static void SetSubtitle(AudioSource source)
+        {
+            AudioSourceSubtitle.Instance.Add(source);
+        }
+    }
+}
---
+++ PH_Subtitles/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PH_Subtitles")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PH_Subtitles")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3A05B6BD-B627-4A3E-B854-FE18C2B6E1F1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
